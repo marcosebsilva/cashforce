4b8bc896386d2b10b1493c3b6e525b255efff2b4
Marcos <dusdoom.dev@gmail.com>
2024-08-03 15:57:48 -0300

    feat: created base NavBar item + mocked values

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,47 +1,69 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import UserNfeTable from './components/UserNfeTable.vue';
+  import NavBarItem from './components/NavBarItem.vue';
+  import icon from './assets/handshake_icon.svg';
+
+  const content = [
+    {
+      value: {
+        nota_fiscal: '1234',
+        sacado: 'Sacado 001',
+        cedente: 'Cedente 002',
+        emissao: '12/10/2020',
+        valor: 'R$ 49.725,00',
+        status: 'Recebido',
+        button: 'Dados do cedente'
+      },
+    },
+    {
+      value: {
+        nota_fiscal: '1234',
+        sacado: 'Sacado 002',
+        cedente: 'Cedente 003',
+        emissao: '12/10/2020',
+        valor: 'R$ 49.725,00',
+        status: 'Recebido',
+        button: 'Dados do cedente'
+      },
+    },
+    {
+      value: {
+        nota_fiscal: '1234',
+        sacado: 'Sacado 003',
+        cedente: 'Cedente 004',
+        emissao: '12/10/2020',
+        valor: 'R$ 49.725,00',
+        status: 'Recebido',
+        button: 'Dados do cedente'
+      },
+    }
+  ]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <div class="flex h-screen w-screen">
+    <div class="bg-sideslider-grey min-w-72">
+      <div class="py-[25px] px-6">
+        <img alt="Cashforce logo" src="./assets/logo_full.svg" width="160" />
+      </div>
+      <nav>
+        <NavBarItem :icon="icon" title="Home" selected />
+      </nav>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="w-full bg-white shadow-xl rounded-l-xl divide-y divide-solid divide-n-mid">
+      <div class="h-16"></div>
+      <div class="px-12 pt-10">
+        <header class="flex flex-col gap-1.5 mb-5">
+          <div class="flex gap-2.5">
+            <img alt="Handshake icon" src="./assets/handshake_icon.svg">
+            <h1 class="text-2xl font-bold leading-7 text-dark-blue">Notas fiscais</h1>
+          </div>
+          <h3 class="text-n-dark-100">Visualize as notas fiscais que você tem.</h3>
+        </header>
+        <main>
+          <UserNfeTable :content="content" />
+        </main>
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ frontend/src/components/NavBarItem.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+    defineProps<{
+        icon: string,
+        title: string,
+        selected: boolean,
+    }>()
+</script>
+<template>
+    <div class="flex w-full cursor-pointer hover:bg-gray-100">
+        <div class="w-1 h-20 bg-cash-green rounded-xl"></div>
+        <div class="flex py-6 pl-5 gap-4 w-full items-center">
+            <img :src="icon" class="green_filter">
+            <h1 class="w-fit text-nowrap text-md p-0 font-medium text-cash-green">Notas fiscais</h1>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+    .green_filter {
+        filter: invert(49%) sepia(68%) saturate(3433%) hue-rotate(139deg) brightness(95%) contrast(101%);
+    }
+</style>
