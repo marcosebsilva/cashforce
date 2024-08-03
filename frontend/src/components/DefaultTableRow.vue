b713ec226292b312d8543654feb4c03d67db0774
Marcos <dusdoom.dev@gmail.com>
2024-08-03 14:25:11 -0300

    feat: created default table row

---
+++ frontend/src/components/DefaultTableRow.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    value: object,
+    style?: string,
+  }>();
+
+  const defaultContentStyle = 'text-n-dark-300 font-medium text-sm';
+</script>
+
+<template>
+  <tr class="hover:bg-gray-100 cursor-pointer">
+    <td v-for="(item, keyIndex) in Object.values(props.value)"
+      :class="'pl-8 py-2.5 border-y first:border-l first:rounded-l-lg last:rounded-r-lg last:border-r self-end ' + (props.style || defaultContentStyle)"
+      :key="keyIndex">
+      <slot name="cell" :value=item :key-index="keyIndex">
+        {{ item }}
+      </slot>
+    </td>
+  </tr>
+</template>
