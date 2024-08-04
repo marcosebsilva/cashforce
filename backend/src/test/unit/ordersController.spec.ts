85caaded76d1dda66acd9582e9e7a260369785a8
Marcos <dusdoom.dev@gmail.com>
2024-08-04 15:06:22 -0300

    fix: fixed build config for jest files

--- backend/src/test/unit/ordersController.spec.ts
+++ backend/src/test/unit/ordersController.spec.ts
@@ -1,7 +1,6 @@
 import { OrdersController } from "../../controllers/ordersController";
 import { OrdersService } from "../../services/ordersService";
 import { NextFunction, Request, Response } from "express";
-import "@types/jest";
 
 describe("OrdersController", () => {
     const mockService = new OrdersService();
